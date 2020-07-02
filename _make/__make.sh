2ef81cc91ef7a7c02ad4a1ff79d060f1b214dd20
david reinfurt <reinfurt@o-r-g.com>
2020-07-02 12:09:28 -0400

    update _make/__make.sh to work on ubuntu, ad processing-java cmd line tools, update readme

--- _make/__make.sh
+++ _make/__make.sh
@@ -10,28 +10,28 @@ for f in *.wav
     do
         echo $f >> __list.txt
         ffmpeg -i "$f" -acodec pcm_s16le -ar 16000 _"$f"
-done
+    done
+
+# get absolute path
+PATH_TO=$(pwd)
 
 # run processing for *.wav
 # crop resulting mov
-# generate animated .gif
+# ? generate animated .gif ** todo **
 for f in _*.wav
     do 
         echo "Processing ******** '$f'"
         cp "$f" ../data/in.wav
-        pjava ../spectrogram/.
-        ffmpeg -i ../spectrogram/out/in-spectrogram.mp4 -filter:v "crop=720:720:0:720" ../spectrogram/out/"$f".mp4
+        # pjava ../spectrogram/.
+        processing-java --sketch=$PATH_TO/../spectrogram --run
+        ffmpeg -i $PATH_TO/../spectrogram/out/in-spectrogram.mp4 -filter:v "crop=720:720:0:720" $PATH_TO/../spectrogram/out/"$f".mp4
         rm ../spectrogram/out/in-spectrogram.mp4
 done
 
 # cleanup 
 rm __list.txt
 rm _*.wav
-open -a "/Applications/Quicktime Player.app" ../spectrogram/out/*.mp4
+# open -a "/System/Applications/Quicktime Player.app" ../spectrogram/out/*.mp4
 echo "done."
-
-# ** to do **
-# generate animated gif?
-# although perhaps better if we hear the sounds on www, so .mov
 
 exit
--- readme.txt
+++ readme.txt
@@ -3,10 +3,32 @@ June 25, 2020
 
 --
 
-requires processing, available here
+takes any number of .wav files in /_make/*.wav, converts to 16-bit, 
+then runs processing script to generate spectrogram of audio
+written to .mp4 in spectrogram/out/*.mp4
+
++ requires java, see:
+
+    * https://www.digitalocean.com/community/tutorials/how-to-install-java-with-apt-on-ubuntu-18-04
+
++ requires processing, available here
 
 	* https://processing.org/download/
 
-and requires installing processing command line tools
++ requires processing-java command line tool (included) 
+  which should be installed here: 
 
-and requires pjava installed, which is a custom wrapper on processing command line tools.
+    > cp processing-java /usr/local/bin/
+    > chmod +x /usr/local/bin/processing-java
+
++ requires ffmpeg, available via apt-get
+
+    > sudo apt update
+    > sudo apt install ffmpeg
+
++ after installs, logout and login to update env vars
+
++ this package runs via a bash script. run like so:
+
+    > ./_make/__make.sh
+
