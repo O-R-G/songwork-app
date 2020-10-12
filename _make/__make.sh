95a1ccca46ee7a75442e101f81269134ee0cfe1a
Wei-Hao Wang <wwang03@risd.edu>
2020-10-12 22:51:02 +0800

    execute upload-finish.php

--- _make/__make.sh
+++ _make/__make.sh
@@ -13,6 +13,7 @@ for f in 0*.wav 0*.mp3
     do
         echo $f >> __list.txt
         ffmpeg -i "$f" -acodec pcm_s16le -ar 16000 _"$f"
+        rm $f
     done
 
 # get absolute path
@@ -44,13 +45,14 @@ for f in _*.wav _*.mp3
         # 280 = 960 / 1.5 - 360
 	ffmpeg -i $PATH_TO/../spectrogram/out/"$filename".mp4 -vframes 1 -an -s 360x360 -ss 6 /var/www/html/media/placeholder/"$filename".png
 	# move example.mp4 to media/
-	mv $PATH_TO/../spectrogram/out/"$filename".mp4 /var/www/html/media/
-	# move example.wav to media/original-audio/
+	mv $PATH_TO/../spectrogram/out/"$filename".mp4 /var/www/html/media/--"$filename".mp4
+	# move example.wav to media/audio/
 	mv $filename.$extension /var/www/html/media/audio/
 	# remove _example.wav
 	rm $f
     rm ../spectrogram/out/in-spectrogram.mp4
 done
+php /var/www/html/views/upload-finish.php
 # cleanup 
 rm __list.txt
 # rm _*.wav
