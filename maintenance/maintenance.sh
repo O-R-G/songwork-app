c6773dcaa9644b386ccec3181e2f65c07bb9cca0
Wei-Hao Wang <wwang03@risd.edu>
2021-03-09 13:56:37 +0800

    update maintenance

--- maintenance/maintenance.sh
+++ maintenance/maintenance.sh
@@ -7,18 +7,31 @@ if pgrep -x "$SERVICE" >/dev/null
 then
     if test -f "$LIST_FILE"
 	then
-	    echo "__make.sh is running and __list.txt exists."
+	    echo "[O] __make.sh is running and __list.txt exists."
 	else 
-		echo "!!error!! __make.sh is running but __list.txt doesnt exist."
+		echo "[error] __make.sh is running but __list.txt doesnt exist."
+		echo "        Killing process $SERVICE_PID ..."
 		kill -9 "$SERVICE_PID"
+		echo "        Removing .mp4 in spectrogram/out/ ..."
+		rm -rf /var/www/app/songwork-app/spectrogram/out/*.mp4
+		echo "        Removing *wav* in data/ ..."
+	    rm -rf /var/www/app/songwork-app/data/*wav*
+	    echo "        Executing __make.sh again ..."
+	    bash "$MAKE_FILE"
 	fi
 else
     if test -f "$LIST_FILE"
 	then
-	    echo "!!error!! __make.sh is not running but __list.txt exists."
+	    echo "[error] __make.sh is not running but __list.txt exists."
+	    echo "        Removing __list.txt ..."
 	    rm -rf "$LIST_FILE"
+	    echo "        Removing .mp4 in spectrogram/out/ ..."
+		rm -rf /var/www/app/songwork-app/spectrogram/out/*.mp4
+		echo "        Removing *wav* in data/ ..."
+	    rm -rf /var/www/app/songwork-app/data/*wav*
+	    echo "        Executing __make.sh again ..."
 	    bash "$MAKE_FILE"
 	else 
-		echo "__make.sh is not running and __list.txt doesnt exist."
+		echo "[O] __make.sh is not running and __list.txt doesnt exist."
 	fi
 fi
