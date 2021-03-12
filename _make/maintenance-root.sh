173a57e3945bf7d1461d970869996bcfaf7ac9fb
Wei-Hao Wang <wwang03@risd.edu>
2021-03-12 17:19:32 +0800

    clean maintenance.sh

--- _make/maintenance-root.sh
+++ _make/maintenance-root.sh
@@ -17,6 +17,7 @@ then
 	then
 	    echo "[O] __make.sh is running and __list.txt exists."
 	else 
+		RUNAGAIN_MAKE=1
 		echo "[error] __make.sh is running but __list.txt doesnt exist."
 		# cd "$_MAKE_LOCATION"
 		echo "        Killing process $SERVICE_PID ..."
@@ -25,15 +26,16 @@ then
 		rm -rf /var/www/app/songwork-app/spectrogram/out/*.mp4
 		echo "        Removing *wav* in data/ ..."
 	    rm -rf /var/www/app/songwork-app/data/*wav*
-	    echo "        Executing __make.sh again ..."
+	    # echo "        Executing __make.sh again ..."
 	    #cd "$_MAKE_LOCATION"
 	    # bash "$MAKE_FILE" 1>>debug.log 2>&1 &
-	    runuser -l reinfurt -c './__make.sh 1>>debug.log 2>&1 &'
+	    # runuser -l reinfurt -c './__make.sh 1>>debug.log 2>&1 &'
 	    # php submit-response.php
 	fi
 else
     if test -f "$LIST_FILE"
 	then
+		RUNAGAIN_MAKE=1
 	    echo "[error] __make.sh is not running but __list.txt exists."
 	    # cd "$_MAKE_LOCATION"
 	    echo "        Removing __list.txt ..."
@@ -42,11 +44,11 @@ else
 		rm -rf /var/www/app/songwork-app/spectrogram/out/*.mp4
 		echo "        Removing *wav* in data/ ..."
 	    rm -rf /var/www/app/songwork-app/data/*wav*
-	    echo "        Executing __make.sh again ..."
+	    # echo "        Executing __make.sh again ..."
 	    #cd "$_MAKE_LOCATION"
 	    # bash "$MAKE_FILE" 1>>debug.log 2>&1 &
 	    # bash ./__make.sh 1>>debug.log 2>&1 &
-	    runuser -l reinfurt -c './__make.sh 1>>debug.log 2>&1 &'
+	    # runuser -l reinfurt -c './__make.sh 1>>debug.log 2>&1 &'
 	    # php submit-response.php
 	else 
 		echo "[O] __make.sh is not running and __list.txt doesnt exist."
--- _make/maintenance.sh
+++ _make/maintenance.sh
@@ -10,45 +10,11 @@ whoami
 date
 cd "$_MAKE_LOCATION"
 pwd
-
-if pgrep -x "$SERVICE" >/dev/null
+if "$RUNAGAIN_MAKE"
 then
-    if test -f "$LIST_FILE"
-	then
-	    echo "[O] __make.sh is running and __list.txt exists."
-	else 
-		echo "[error] __make.sh is running but __list.txt doesnt exist."
-		# cd "$_MAKE_LOCATION"
-		# echo "        Killing process $SERVICE_PID ..."
-		# kill -9 "$SERVICE_PID"
-		# echo "        Removing .mp4 in spectrogram/out/ ..."
-		# rm -rf /var/www/app/songwork-app/spectrogram/out/*.mp4
-		# echo "        Removing *wav* in data/ ..."
-	    # rm -rf /var/www/app/songwork-app/data/*wav*
-	    echo "        Executing __make.sh again ..."
-	    # cd "$_MAKE_LOCATION"
-	    # bash "$MAKE_FILE" 1>>debug.log 2>&1 &
-	    ./__make.sh 1>>debug.log 2>&1 &
-	    # php submit-response.php
-	fi
-else
-    if test -f "$LIST_FILE"
-	then
-	    echo "[error] __make.sh is not running but __list.txt exists."
-	    # cd "$_MAKE_LOCATION"
-	    # echo "        Removing __list.txt ..."
-	    # rm -rf "$LIST_FILE"
-	    # echo "        Removing .mp4 in spectrogram/out/ ..."
-		# rm -rf /var/www/app/songwork-app/spectrogram/out/*.mp4
-		# echo "        Removing *wav* in data/ ..."
-	    # rm -rf /var/www/app/songwork-app/data/*wav*
-	    echo "        Executing __make.sh again ..."
-	    #cd "$_MAKE_LOCATION"
-	    # bash "$MAKE_FILE" 1>>debug.log 2>&1 &
-	    # bash ./__make.sh 1>>debug.log 2>&1 &
-	    ./__make.sh 1>>debug.log 2>&1 &
-	    # php submit-response.php
-	else 
-		echo "[O] __make.sh is not running and __list.txt doesnt exist."
-	fi
+	echo "        Executing __make.sh again ..."
+    # cd "$_MAKE_LOCATION"
+    # bash "$MAKE_FILE" 1>>debug.log 2>&1 &
+    ./__make.sh 1>>debug.log 2>&1 &
+    RUNAGAIN_MAKE=0
 fi
